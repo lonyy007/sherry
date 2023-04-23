86d0ddc4123bd839e84325934795454211938fe2
lonyy007 <lonyy007@mymail.unisa.edu.au>
2023-04-24 03:25:33 +0930

    Create a temp array and use loop

--- src/task4/Car.java
+++ src/task4/Car.java
@@ -6,6 +6,40 @@ public class Car {
 	private String color;
 	
 	public Car(String make, String model, String color) {
-		
+		this.make=make;
+		this.model=model;
+		this.color=color;
+	}
+
+
+
+	public String getMake() {
+		return make;
+	}
+
+	public void setMake(String make) {
+		this.make = make;
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+
+	@Override
+	public String toString() {
+		return String.format("Car{make:%s,model:%s,color:%s}",this.make,this.model,this.color);
 	}
 }
--- src/task4/CarProcessing.java
+++ src/task4/CarProcessing.java
@@ -1,30 +1,65 @@
 package task4;
 
 public class CarProcessing {
-	public static void main(String[] args) {
-		
-		CarProcessing cp = new CarProcessing();
-		
-		Car[] cars = { 
-				new Car("Honda", "Civic", "Red"),
-				new Car("Holden", "Civic", "Red"),
-				new Car("Jeep", "Wrangler", "Blue"),
-				new Car("Jeep", "Wrangler", "Green"),
-				new Car("Holden", "Falcon", "Yellow"),
-				new Car("Honda", "Civic", "Blue"),
-				new Car("Honda", "Falcon", "Blue"),
-				new Car("Honda", "Civic", "Purple")
-		};
-		
-		Car[] noDuplicates = cp.removeDuplicates(cars);
-		
-		for(Car c : noDuplicates ) {
-			System.out.println(c);
-		}
-	}
-	
-	public Car[] removeDuplicates(Car[] inputArray) {
-		return null;
-	}
-	
+    public static void main(String[] args) {
+
+        CarProcessing cp = new CarProcessing();
+
+        Car[] cars = {
+                new Car("Honda", "Civic", "Red"),
+                new Car("Holden", "Civic", "Red"),
+                new Car("Holden", "Civic", "Red"),
+                new Car("Jeep", "Wrangler", "Blue"),
+                new Car("Jeep", "Wrangler", "Green"),
+                new Car("Jeep", "Wrangler", "Green"),
+                new Car("Jeep", "Wrangler", "Green"),
+                new Car("Holden", "Falcon", "Yellow"),
+                new Car("Honda", "Civic", "Blue"),
+                new Car("Honda", "Civic", "Blue"),
+                new Car("Honda", "Falcon", "Blue"),
+                new Car("Honda", "Civic", "Purple")
+        };
+
+        Car[] noDuplicates = cp.removeDuplicates(cars);
+
+        for (Car c : noDuplicates) {
+            System.out.println(c);
+        }
+    }
+
+
+    public Car[] removeDuplicates(Car[] inputArray) {
+
+
+        Car[] temp=new Car[inputArray.length];
+
+        int index=0;
+
+
+        for (int i = 0; i < inputArray.length; i++) {
+
+            boolean flag=true;
+
+            for (int j = 0; j < index; j++) {
+
+                if (inputArray[i].getModel().equals(temp[j].getModel())&&inputArray[i].getColor().equals(temp[j].getColor())){
+                    flag=false;
+                    break;
+                }
+            }
+
+            if (flag){
+                temp[index]=inputArray[i];
+                index++;
+            }
+        }
+
+        Car[] result=new Car[index];
+
+        for (int i = 0; i < index; i++) {
+            result[i]=temp[i];
+        }
+        return result;
+    }
+
 }
